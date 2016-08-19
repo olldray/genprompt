1bcea686604187b7e2b10f2caacdb69e64b8aaff
Thomas Allen <olldray@gmail.com>
2016-08-19 08:54:16 -0700

    empty Prompt class

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
+import Data.Default ( Default(..))
 import Lib
 
+data Wrapper = Wrapper Int
+    deriving (Show)
+
+instance Default Wrapper where
+    def = Wrapper 23
+instance Prompt Wrapper where
+
 main :: IO ()
-main = someFunc
+main = do
+    laddend <- prompt
+    raddend <- prompt
+    print $ go laddend raddend
+    where go :: Wrapper -> Wrapper -> Wrapper
+          go (Wrapper x) (Wrapper y) = Wrapper (x+y)
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,19 @@
+{-# LANGUAGE DefaultSignatures #-}
+
 module Lib
     ( someFunc
+    , Prompt (..)
     ) where
+
+import Data.Default ( Default(..))
+
+
+class Prompt a where
+    prompt :: IO a
+
+    default prompt :: (Default a) => IO a
+    prompt = return def
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
